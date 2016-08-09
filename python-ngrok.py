b04c729a27e041f1b6804988254fa01e05f89ab0
hauntek <hauntek@hotmail.com>
2016-08-10 01:40:10 +0800

    Update python-ngrok.py

--- python-ngrok.py
+++ python-ngrok.py
@@ -1,4 +1,5 @@
 # 建议Python 3.1 以上运行 以下是依赖
+# 项目地址: https://github.com/hauntek/python-ngrok
 import socket
 import select
 import ssl
@@ -8,11 +9,11 @@ import random
 import time
 import threading
 
-host = 'server.ngrok.cc' #ngrok服务器地址
-port = 4443 #端口
-bufsize = 1024
+host = 'ittun.com' # Ngrok服务器地址
+port = 44433 # 端口
+bufsize = 1024 # 吞吐量
 
-Tunnels = list() #全局渠道赋值
+Tunnels = list() # 全局渠道赋值
 body = dict()
 body['protocol'] = 'http'
 body['hostname'] = 'www.xxx.com'
@@ -20,7 +21,7 @@ body['subdomain'] = ''
 body['rport'] = 0
 body['lhost'] = '127.0.0.1'
 body['lport'] = 80
-Tunnels.append(body) #加入渠道队列
+Tunnels.append(body) # 加入渠道队列
 
 body = dict()
 body['protocol'] = 'http'
@@ -29,7 +30,7 @@ body['subdomain'] = 'xxx'
 body['rport'] = 0
 body['lhost'] = '127.0.0.1'
 body['lport'] = 80
-Tunnels.append(body) #加入渠道队列
+Tunnels.append(body) # 加入渠道队列
 
 body = dict()
 body['protocol'] = 'tcp'
@@ -38,19 +39,30 @@ body['subdomain'] = ''
 body['rport'] = 55499
 body['lhost'] = '127.0.0.1'
 body['lport'] = 22
-Tunnels.append(body) #加入渠道队列
+Tunnels.append(body) # 加入渠道队列
 
 mainsocket = 0
 
-inputs = list()
-outputs = list()
-
-timeout = 1
-
-socklist = list()
-
 ClientId = ''
 pingtime = 0
+
+def getloacladdr(Tunnels, Url):
+    protocol = Url[0:Url.find(':')]
+    hostname = Url[Url.find('//') + 2:]
+    subdomain = hostname[0:hostname.find('.')]
+    rport = Url[Url.rfind(':') + 1:]
+
+    for tunnelinfo in Tunnels:
+        if tunnelinfo.get('protocol') == protocol:
+            if tunnelinfo.get('hostname') == hostname:
+                return tunnelinfo
+            if tunnelinfo.get('subdomain') == subdomain:
+                return tunnelinfo
+            if tunnelinfo.get('protocol') == 'tcp':
+                if tunnelinfo.get('rport') == int(rport):
+                    return tunnelinfo
+
+    return dict()
 
 def dnsopen(host):
     try:
@@ -91,6 +103,7 @@ def NgrokAuth():
     Payload['Version'] = '2'
     Payload['MmVersion'] = '1.7'
     Payload['User'] = 'user'
+    Payload['Password'] = ''
     body = dict()
     body['Type'] = 'Auth'
     body['Payload'] = Payload
@@ -138,14 +151,14 @@ def lentobyte1(len):
     xx1 = struct.pack('L', 0)
     return xx + xx1
 
-def sendbuf(sock, buf, isblock=True):
+def sendbuf(sock, buf, isblock = True):
     if isblock:
         sock.setblocking(1)
     sock.send(buf)
     if isblock:
         sock.setblocking(0)
 
-def sendpack(sock, msg, isblock=True):
+def sendpack(sock, msg, isblock = True):
     if isblock:
         sock.setblocking(1)
     sock.send(lentobyte1(len(msg)) + msg.encode('utf-8'))
@@ -162,111 +175,51 @@ def getRandChar(length):
     _chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz"
     return ''.join(random.sample(_chars, length))
 
-def setsocklist(sock, linkstate, type, tosock = None):
-    data = dict()
-    data['sock'] = sock
-    data['linkstate'] = linkstate
-    data['type'] = type
-    data['tosock'] = tosock
-    return data
-
-def getloacladdr(Tunnels, url):
-    protocol = url[0:url.find(':')]
-    hostname = url[url.find('//') + 2:]
-    subdomain = hostname[0:hostname.find('.')]
-    rport = url[url.rfind(':') + 1:]
-
-    for tunnelinfo in Tunnels:
-        if tunnelinfo.get('protocol') == protocol:
-            if tunnelinfo.get('hostname') == hostname:
-                return tunnelinfo
-            if tunnelinfo.get('subdomain') == subdomain:
-                return tunnelinfo
-            if tunnelinfo.get('protocol') == 'tcp':
-                if tunnelinfo.get('rport') == int(rport):
-                    return tunnelinfo
-
-    return dict()
-
-# 客户端程序初始化
-if __name__ == '__main__':
-    print('python-ngrok v1.0')
-    mainsocket = connectremote(host, port)
-    if mainsocket:
-        socklist.append(setsocklist(mainsocket, 0, 1))
+# 客户端程序处理过程
+def HKClient(sock, linkstate, type, tosock = None):
+    global ClientId
+    global pingtime
+    recvbuf = bytes()
     while True:
-
-        # 检测控制连接是否连接.
-        if mainsocket == False:
-            ip = dnsopen(host)
-            if ip == False:
-                print('update dns')
-                time.sleep(10)
-                continue
-            mainsocket = connectremote(ip, port)
-            if mainsocket == False:
-                print('connect failed...!')
-                time.sleep(10)
-                continue
-            socklist.append(setsocklist(mainsocket, 0, 1))
-
-        # 发送心跳
-        if pingtime + 25 < time.time() and pingtime != 0:
-            sendpack(mainsocket, Ping())
-            pingtime = time.time()
-
-        # 重新赋值
         inputs = list()
         outputs = list()
-        for k, z in enumerate(socklist):
-            if isinstance(z['sock'], socket.socket):
-                inputs.append(z['sock'])
-                if z['linkstate'] == 0:
-                    outputs.append(z['sock'])
-            else:
-                if z['sock'] == 1:
-                    mainsocket = False
-                socklist.pop(k)
+        if isinstance(sock, socket.socket):
+            inputs.append(sock)
+            if linkstate == 0:
+                outputs.append(sock)
+        else:
+            if sock == 1:
+                mainsocket = False
+            print('z:close')
+        try:
+            readable , writable , exceptional = select.select(inputs, outputs, [], 1)
+        except select.error:
+            print('select.error')
 
-        readable , writable , exceptional = select.select(inputs, outputs, [], timeout) # 堵塞查询，比较耗时
-        if not (readable or writable):
-            continue
-
-        for k, sockinfo in enumerate(socklist):
-            sock = sockinfo['sock']
-            # 可读
-            if sock in readable:
-                try:
-                    recvbut = sock.recv(bufsize)
-                except socket.error:
-                    break
-
-                if not recvbut or len(recvbut) == 0:
-                    if sockinfo['type'] == 1:
-                        mainsocket = False
-
-                    socklist.pop(k)
-                    continue
+        # 可读
+        if readable:
+            try:
+                recvbut = sock.recv(bufsize)
+                if not recvbut: break
 
                 if len(recvbut) > 0:
-                    if not sockinfo.get('recvbuf'):
-                        sockinfo['recvbuf'] = recvbut
+                    if not recvbuf:
+                        recvbuf = recvbut
                     else:
-                        sockinfo['recvbuf'] += recvbut
-                    socklist[k] = sockinfo
-                #print('[%s] %s' % (k, sockinfo['recvbuf']))
-                if sockinfo['type'] == 1 or (sockinfo['type'] == 2 and sockinfo['linkstate'] == 1):
-                    allrecvbut = sockinfo['recvbuf']
-                    lenbyte = tolen1(allrecvbut[0:4])
-                    if len(allrecvbut) >= (8 + lenbyte):
-                        buf = allrecvbut[8:].decode('utf-8')
+                        recvbuf += recvbut
+
+                if type == 1 or (type == 2 and linkstate == 1):
+                    lenbyte = tolen1(recvbuf[0:4])
+                    if len(recvbuf) >= (8 + lenbyte):
+                        buf = recvbuf[8:].decode('utf-8')
                         js = json.loads(buf)
                         print(js)
-                        if sockinfo['type'] == 1:
+                        if type == 1:
                             if js['Type'] == 'ReqProxy':
                                 newsock = connectremote(host, port)
                                 if newsock:
-                                    socklist.append(setsocklist(newsock, 0, 2))
+                                    thread = threading.Thread(target = HKClient, args = (newsock, 0, 2))
+                                    thread.start()
                             if js['Type'] == 'AuthResp':
                                 ClientId = js['Payload']['ClientId']
                                 sendpack(sock, Ping())
@@ -281,59 +234,83 @@ if __name__ == '__main__':
                                 else:
                                     print('Add tunnel ok,type:%s url:%s' % (js['Payload']['Protocol'], js['Payload']['Url']))
 
-                        if sockinfo['type'] == 2:
-                            if sockinfo['linkstate'] == 1:
+                        if type == 2:
+                            if linkstate == 1:
                                 if js['Type'] == 'StartProxy':
                                     loacladdr = getloacladdr(Tunnels, js['Payload']['Url'])
 
                                     newsock = connectlocal(loacladdr['lhost'], loacladdr['lport'])
                                     if newsock:
-                                        socklist.append(setsocklist(newsock, 0, 3, sock))
-                                        sockinfo['tosock'] = newsock
-                                        sockinfo['linkstate'] = 2
+                                        thread = threading.Thread(target = HKClient, args = (newsock, 0, 3, sock))
+                                        thread.start()
+                                        tosock = newsock
+                                        linkstate = 2
                                     else:
-                                        body = '<!DOCTYPE html><html><head><meta charset="utf-8"><title>Web服务错误</title><meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no"><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><style>html,body{height:100%%}body{margin:0;padding:0;width:100%%;display:table;font-weight:100;font-family:"Microsoft YaHei",Arial,Helvetica,sans-serif}.container{text-align:center;display:table-cell;vertical-align:middle}.content{border:1px solid #ebccd1;text-align:center;display:inline-block;background-color:#f2dede;color:#a94442;padding:30px}.title{font-size:18px}.copyright{margin-top:30px;text-align:right;color:#000}</style></head><body><div class="container"><div class="content"><div class="title">隧道 %s 无效<br>无法连接到<strong>%s</strong>. 此端口尚未提供Web服务</div><div class="copyright">Powered By python-ngrok</div></div></div></body></html>'
+                                        body = '<!DOCTYPE html><html><head><meta charset="utf-8"><title>Web服务错误</title><meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no"><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><style>html,body{height:100%%}body{margin:0;padding:0;width:100%%;display:table;font-weight:100;font-family:"Microsoft YaHei",Arial,Helvetica,sans-serif}.container{text-align:center;display:table-cell;vertical-align:middle}.content{border:1px solid #ebccd1;text-align:center;display:inline-block;background-color:#f2dede;color:#a94442;padding:30px}.title{font-size:18px}.copyright{margin-top:30px;text-align:right;color:#000}</style></head><body><div class="container"><div class="content"><div class="title">隧道 %s 无效<br>无法连接到<strong>%s</strong>. 此端口尚未提供Web服务</div></div></div></body></html>'
                                         html = body % (js['Payload']['Url'], loacladdr['lhost'] + ':' + str(loacladdr['lport']))
                                         header = "HTTP/1.0 502 Bad Gateway" + "\r\n"
-                                        header += "Server: python-ngrok" + "\r\n"
                                         header += "Content-Type: text/html" + "\r\n"
                                         header += "Content-Length: %d" + "\r\n"
                                         header += "\r\n" + "%s"
                                         buf = header % (len(html), html)
                                         sendbuf(sock, buf.encode('utf-8'))
 
-                        if len(allrecvbut) == (8 + lenbyte):
-                            sockinfo['recvbuf'] = b''
+                        if len(recvbuf) == (8 + lenbyte):
+                            recvbuf = bytes()
                         else:
-                            sockinfo['recvbuf'] = allrecvbut[8 + lenbyte:]
-                        socklist[k] = sockinfo
+                            recvbuf = recvbuf[8 + lenbyte:]
 
-                #print('[%s] %s' % (k, sockinfo.get('recvbuf')))
-                if sockinfo['type'] == 3 or (sockinfo['type'] == 2 and sockinfo['linkstate'] == 2):
-                    if not sockinfo.get('recvbuf'):
-                        continue
-                    #if sockinfo['type'] == 2 and sockinfo['linkstate'] == 2:
-                        #print('发送给本地')
-                        #print(sockinfo.get('recvbuf'))
-                    #if sockinfo['type'] == 3:
-                        #print('发送给远程')
-                        #print(sockinfo.get('recvbuf'))
-                    #print('[%s] %s' % (k, sockinfo.get('recvbuf')))
-                    sendbuf(sockinfo['tosock'], sockinfo['recvbuf'])
-                    sockinfo['recvbuf'] = b''
-                    socklist[k] = sockinfo
+                if type == 3 or (type == 2 and linkstate == 2):
+                    sendbuf(tosock, recvbuf)
+                    recvbuf = bytes()
 
-            # 可写
-            if sock in writable:
-                if sockinfo['linkstate'] == 0:
-                    if sockinfo['type'] == 1:
-                        sendpack(sock, NgrokAuth(), False)
-                        sockinfo['linkstate'] = 1
-                        socklist[k] = sockinfo
-                    if sockinfo['type'] == 2:
-                        sendpack(sock, RegProxy(ClientId), False)
-                        sockinfo['linkstate'] = 1
-                        socklist[k] = sockinfo
-                    if sockinfo['type'] == 3:
-                        sockinfo['linkstate'] = 1
-                        socklist[k] = sockinfo
+            except socket.error:
+                print('socket.error')
+                break
+
+        # 可写
+        if writable:
+            if linkstate == 0:
+                if type == 1:
+                    sendpack(sock, NgrokAuth(), False)
+                    linkstate = 1
+                if type == 2:
+                    sendpack(sock, RegProxy(ClientId), False)
+                    linkstate = 1
+                if type == 3:
+                    linkstate = 1
+
+    if type == 1:
+        mainsocket = False
+    # if type == 3:
+    #     tosock.close()
+    sock.close()
+
+# 客户端程序初始化
+if __name__ == '__main__':
+    print('python-ngrok v1.2')
+    mainsocket = connectremote(host, port)
+    if mainsocket:
+        thread = threading.Thread(target = HKClient, args = (mainsocket, 0, 1))
+        thread.start()
+    while True:
+
+        # 检测控制连接是否连接.
+        if mainsocket == False:
+            ip = dnsopen(host)
+            if ip == False:
+                print('update dns')
+                time.sleep(10)
+                continue
+            mainsocket = connectremote(ip, port)
+            if mainsocket == False:
+                print('connect failed...!')
+                time.sleep(10)
+                continue
+            thread = threading.Thread(target = HKClient, args = (mainsocket, 0, 1))
+            thread.start()
+
+        # 发送心跳
+        if pingtime + 25 < time.time() and pingtime != 0:
+            sendpack(mainsocket, Ping())
+            pingtime = time.time()

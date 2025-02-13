548556b70fecd040f357b585f40abb12fe833f49
Bernardo Arocho <hauntek@hotmail.com>
2025-02-14 06:26:23 +0800

    Update python-ngrok_deepseek.py

--- python-ngrok_deepseek.py
+++ python-ngrok_deepseek.py
@@ -10,7 +10,7 @@ import threading
 import asyncio
 
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     format='[%(asctime)s] [%(levelname)s] [%(name)s] %(message)s',
     datefmt='%Y/%m/%d %H:%M:%S'
 )
